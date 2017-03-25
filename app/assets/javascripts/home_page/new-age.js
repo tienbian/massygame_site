4e1f344ec0816a75a7df0df77e86940ac9349d70
at-tientvq <tientranvu@gmail.com>
2017-03-25 23:44:14 +0700

    add hover and fade action

--- app/assets/javascripts/home_page/new-age.js
+++ app/assets/javascripts/home_page/new-age.js
@@ -28,4 +28,26 @@
         }
     })
 
+    
 })(jQuery); // End of use strict
+$(document).ready(function() {
+
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    $('.picture-fade').click( function () {
+        modal.style.display = "block";
+        modalImg.src = $(this).find('img').attr('src');
+        captionText.innerHTML = "testing";
+    });
+
+    // When the user clicks on <span> (x), close the modal
+    $('.close').click( function() {
+        modal.style.display = "none";
+    });
+
+}); 
+    
