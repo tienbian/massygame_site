052dfbfc4f2a6c44d3054a24ff14aab2d5bf0548
at-tientvq <tientranvu@gmail.com>
2017-08-02 12:04:13 +0700

    fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,7 +10,7 @@ worker_processes 2
 timeout 30
 preload_app true
 
-listen '/tmp/unicorn.spui.sock', backlog: 64
+listen 'home/ryan/massygame_site/shared/sockets/unicorn.sock', backlog: 64
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
