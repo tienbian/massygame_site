b449fc54035be107146c049ced6a234b2dcaa7c1
at-tientvq <tientranvu@gmail.com>
2017-08-02 15:24:18 +0700

    fix unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,38 +1,20 @@
-root = "/home/ryan/massygame_site/current"
-working_directory root
+# set path to application
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
+working_directory app_dir
 
-pid "#{root}/tmp/pids/unicorn.pid"
 
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
-
+# Set unicorn options
 worker_processes 2
-timeout 30
 preload_app true
+timeout 30
 
-listen 'home/ryan/massygame_site/shared/sockets/unicorn.sock', backlog: 64
+# Set up socket location
+listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
 
-before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
+# Logging
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
-end
-
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
-end
-
-# Force the bundler gemfile environment variable to
-# reference the capistrano "current" symlink
-before_exec do |_|
-  ENV['BUNDLE_GEMFILE'] = File.join(root, 'Gemfile')
-end
+# Set master PID location
+pid "#{shared_dir}/pids/unicorn.pid"
